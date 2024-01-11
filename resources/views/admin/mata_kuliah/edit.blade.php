6c1f37fc0270ae79323d84767a46d6995aeee0e5
MAY RISKA <mayriskaputri789@gmail.com>
2024-01-11 10:35:33 +0700

    update

--- resources/views/admin/mata_kuliah/edit.blade.php
+++ resources/views/admin/mata_kuliah/edit.blade.php
@@ -20,31 +20,23 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card p-3">
-<<<<<<< HEAD
-                        <form method="post" action=" route['admin.mata_kuliah.update', $mata_kuliah]">
-=======
-                   <form method="post" action="{{ route('admin.mata_kuliah.update', [$mata_kuliah]) }}">
-
->>>>>>> dev-MayRiska
+                        <form method="post" action="{{ route('admin.mata_kuliah.update', ['mata_kuliah' => $mata_kuliah]) }}" enctype="multipart/form-data">
                             @csrf 
                             @method('put')
                             <div class="form-group row border-bottom pb-4">
                                 <label for="nama_mata_kuliah" class="col-sm-2 col-form-label">NAMA MATA PELAJARAN</label>
                                 <div class="col-sm-10">
                                     <input type="text" class="form-control" name="nama_mata_kuliah"
-                                        value="{{ old('nama_mata_kuliah') }}" id="nama_mata_kuliah">
+                                        value="{{ old('nama_mata_kuliah', $mata_kuliah->nama_mata_kuliah) }}" id="nama_mata_kuliah" placeholder="Nama Mata Pelajaran">
                                 </div>
                             </div>
                             <div class="form-group row border-bottom pb-4">
                                 <label for="sks" class="col-sm-2 col-form-label">SKS</label>
                                 <div class="col-sm-10">
                                     <select class="form-control" name="sks" id="sks">
-                                        <option {{ old('sks') == 1 ? 'selected' : null }} value="1">1</option>
-                                        <option {{ old('sks') == 2 ? 'selected' : null }} value="2">2</option>
-                                        <option {{ old('sks') == 3 ? 'selected' : null }} value="3">3</option>
-                                        <option {{ old('sks') == 4 ? 'selected' : null }} value="4">4</option>
-                                        <option {{ old('sks') == 5 ? 'selected' : null }} value="5">5</option>
-                                        <option {{ old('sks') == 6 ? 'selected' : null }} value="6">6</option>
+                                        @for ($i = 1; $i <= 6; $i++)
+                                            <option {{ old('sks', $mata_kuliah->sks) == $i ? 'selected' : '' }} value="{{ $i }}">{{ $i }}</option>
+                                        @endfor
                                     </select>
                                 </div>
                             </div>
@@ -52,25 +44,21 @@
                                 <label for="semester" class="col-sm-2 col-form-label">SEMESTER</label>
                                 <div class="col-sm-10">
                                     <select class="form-control" name="semester" id="semester">
-                                        <option {{ old('semester') == 1 ? 'selected' : null }} value="1">Ganjil
-                                        </option>
-                                        <option {{ old('semester') == 2 ? 'selected' : null }} value="2">Genap
-                                        </option>
+                                        <option {{ old('semester', $mata_kuliah->semester) == 1 ? 'selected' : '' }} value="1">Ganjil</option>
+                                        <option {{ old('semester', $mata_kuliah->semester) == 2 ? 'selected' : '' }} value="2">Genap</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="form-group row border-bottom pb-4">
-                                <label for="program_study_id" class="col-sm-2 col-form-label">KELAS</label>
+                                <label for="kelas" class="col-sm-2 col-form-label">KELAS</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="kelas" value="{{ old('kelas') }}"
-                                        id="kelas">
+                                    <input type="text" class="form-control" name="kelas" value="{{ old('kelas', $mata_kuliah->kelas) }}" id="kelas" placeholder="Kelas">
                                 </div>
                             </div>
                             <div class="form-group row border-bottom pb-4">
-                                <label for="program_study_id" class="col-sm-2 col-form-label">MATERI</label>
+                                <label for="materi" class="col-sm-2 col-form-label">MATERI</label>
                                 <div class="col-sm-10">
-                                    <input type="file" class="form-control" name="materi" value="{{ old('materi') }}"
-                                        id="materi">
+                                    <input type="file" class="form-control" name="materi" id="materi">
                                 </div>
                             </div>
                             <button type="submit" class="btn btn-success">Save</button>
